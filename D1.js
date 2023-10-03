2c38ad1c2b6bb70a6223394a994d29df57eb72c9
Giorgia Ipsaro Passione <giorgiaipsaropassione@gmail.com>
2023-10-03 08:40:09 +0200

    commit8

--- D1.js
+++ D1.js
@@ -92,8 +92,9 @@ name2 = "John";
 //console.log(name2);//
 console.log(name1 === name2);
 
-name1 = "john";
+/*name1 = "john";
 //console.log(name1);//
 name2 = "john";
 //console.log(name2);//
-console.log(name1 === name2);
+console.log(name1 === name2);*/
+console.log(name1.toolowerCase === name2.toolowerCase);
